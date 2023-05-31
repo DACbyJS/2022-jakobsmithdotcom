c04584f276365a8c4db2a2d7d972e4a945298bbb
DACbyJS <hello@jakobsmith.com>
2023-05-31 12:31:56 -0600

    Changes content

--- pages/index.js
+++ pages/index.js
@@ -9,22 +9,22 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Jakob Smith | Developer</title>
+        <title>Jakob Smith | Creative and Concientious Web Developer</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
       <div className={styles.container}>
         <div className="white-black-border relative z-10 mt-40 w-full max-w-[900px] mx-auto p-6">
           <div className="font-js-math text-center">
-            <h2 className="text-[8ch] -mb-5">Web Development</h2>
+            <h2 className="text-[8ch] -mb-2">Web Development</h2>
             <h2 className="text-[6ch] tracking-[0.35ch]">Services For</h2>
           </div>
           <div className="flex flex-col items-center mt-10 font-overpass font-light uppercase text-[5.5ch] tracking-wider text-center">
             <Link className="mb-8 red line-link" href="/web-designers">
               Designers
             </Link>
-            <Link className="mb-8 yellow line-link" href="/agencies">
-              Agencies
+            <Link className="mb-8 yellow line-link" href="/creatives">
+              Creatives
             </Link>
             <Link className="mb-8 blue line-link" href="/beneficial-businesses">
               Businesses
