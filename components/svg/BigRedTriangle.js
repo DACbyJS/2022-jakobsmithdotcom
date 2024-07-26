ece1c491ab2bc144c79bfaac8c22cb1c9a086087
DACbyJS <hello@jakobsmith.com>
2024-07-26 11:35:10 -0600

    Begins animations of red triangle

--- components/svg/BigRedTriangle.js
+++ components/svg/BigRedTriangle.js
@@ -1,23 +1,38 @@
 import classNames from 'classnames';
+import NextIntersectionObserver from '../layout/NextIntersectionObserver';
 
 const BigRedTriangle = ({ className, svgProps }) => (
-  <div className={classNames('absolute -left-[20%] top-[15%] 2xl:w-[100vw]', className)}>
-    <svg
-      width="2749"
-      height="2342"
-      viewBox="0 0 2749 2342"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      preserveAspectRatio="xMinYMin meet"
+  <NextIntersectionObserver
+    thresholdValue={1}
+    classes={`big-red-triangle-animate-init h-full relative`}
+    topIn="big-red-triangle-rotate-in"
+  >
+    <div
+      className={classNames(
+        'absolute -left-[10%] top-[500px] 2xl:w-[100vw] big-red-triangle',
+        className
+      )}
     >
-      <path
-        d="M2727.69 12.8673L1085.77 2323.05L16.6244 324.482L2727.69 12.8673Z"
-        fill="#FF4747"
-        stroke="#121416"
-        strokeWidth="20"
-      />
-    </svg>
-  </div>
+      <svg
+        className="big-red-triangle-svg"
+        width="2749"
+        height="2342"
+        viewBox="0 0 2749 2342"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="xMinYMin meet"
+        {...svgProps}
+      >
+        <path
+          className="big-red-triangle-path"
+          d="M2727.69 12.8673L1085.77 2323.05L16.6244 324.482L2727.69 12.8673Z"
+          fill="#FF4747"
+          stroke="#121416"
+          strokeWidth="20"
+        />
+      </svg>
+    </div>
+  </NextIntersectionObserver>
 );
 
 export default BigRedTriangle;
