9c69c151b46b0cdd1eb8f0fabb9e24c1fa150d8d
DACbyJS <hello@jakobsmith.com>
2024-07-31 19:05:20 -0600

    upgrades! with fixes for those upgrades!

--- next.config.js
+++ next.config.js
@@ -10,7 +10,6 @@ const nextConfig = {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
-  experimental: { appDir: true, topLevelAwait: true },
   webpack(config) {
     config.experiments = { ...config.experiments, topLevelAwait: true };
     return config;
