5dcb7e1e2d07a0f07e5af90a80a88ce1b1907633
DACbyJS <hello@jakobsmith.com>
2025-06-13 17:07:16 -0600

    feat: implement custom 404 page with grid of boxes and add new page files

--- pages/404.js
+++ pages/404.js
@@ -1,18 +1,67 @@
-// pages/404.js 
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import Head from "next/head";
+import Link from "next/link";
+
+// Libs
+import { v4 as uuidv4 } from "uuid";
+
+// Constants
+import { pageData } from "../lib/constants";
+
+// Custom
+import MuxHero from "../components/belowthefold/MuxHero";
+import BelowTheFold from "../components/layout/BelowTheFold";
+import Spacer from "../components/layout/Spacer";
+import TransBox from "../components/layout/TransBox";
+import WhiteBlackBorderBox from "../components/layout/WhiteBlackBorderBox";
+import GridOfBoxes from "../components/belowthefold/GridOfBoxes";
 
 export default function Custom404() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push('/');
-  }, [router]);
+  // Content for the grid of boxes
+  const gridOfBoxesContent = pageData.map((item) => ({
+    id: uuidv4(),
+    text: (
+      <Link className="underline" href={item.data.href}>
+        {item.title}
+      </Link>
+    ),
+  }));
 
   return (
-    <div className="bg-js-white h-[500px]">
-      <h1>404 - Page Not Found</h1>
-      <p>Redirecting to the home page...</p> 
-    </div>
+    <>
+      <Head>
+        <title>404 | Page Not Found </title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="This page is not found." />
+      </Head>
+
+      <MuxHero
+        playbackId="jnDZcW3XgvTAs1JP7XT4LvOUw02a00Il4Djkwqh00i762g"
+        metadata={{
+          video_id: "multi-color-liquid-looping-bg",
+          video_title: "Multi-color Liquid Looping Video Background",
+        }}
+        words={["404.", "Page.", "Not.", "Found."]}
+        suppressHydrationWarning
+      />
+      <BelowTheFold>
+        <Spacer />
+
+        <TransBox className="mx-auto z-20">
+          <div className="font-js-math text-center">
+            <h2 className="text-[5ch] sm:text-[6ch] lg:text-[8ch] 2xl:text-[11ch]">
+              Index of Site
+            </h2>
+          </div>
+        </TransBox>
+
+        <Spacer />
+
+        <WhiteBlackBorderBox className="mx-auto max-w-[1300px] lg:-mt-20 z-10 pt-10 sm:pt-16">
+          <GridOfBoxes items={gridOfBoxesContent} />
+        </WhiteBlackBorderBox>
+
+        <Spacer />
+      </BelowTheFold>
+    </>
   );
 }
