819380e7a6d88f2bf637a01dc255e4e2fd378916
DACbyJS <hello@jakobsmith.com>
2025-06-15 11:31:12 -0600

    style: standardize import statements and formatting in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,46 +1,46 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 // Custom Components
-import Layout from '../components/layout/Layout';
-import ThemeProvider from '../lib/contexts/ThemeProvider';
+import Layout from "../components/layout/Layout";
+import ThemeProvider from "../lib/contexts/ThemeProvider";
+import Analytics from "../components/head/analytics";
 
 // External Dependencies
-import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
 // Styles
-import '../styles/index.scss';
+import "../styles/index.scss";
 
 // Fonts
-import { fonts } from '../lib/constants';
+import { fonts } from "../lib/constants";
 
 // App
 function App({ Component, pageProps }) {
   const router = useRouter();
 
+  // pageview tracking for Google Analytics
   useEffect(() => {
     const handleRouteChange = (url) => {
       window.dataLayer = window.dataLayer || [];
       window.dataLayer.push({
-        event: 'pageview',
+        event: "pageview",
         page: url,
       });
     };
-
-    router.events.on('routeChangeComplete', handleRouteChange);
-
-    // Initial page load
+    router.events.on("routeChangeComplete", handleRouteChange);
     handleRouteChange(window.location.pathname);
-
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
+      router.events.off("routeChangeComplete", handleRouteChange);
     };
   }, [router.events]);
 
   return (
+    
     <ThemeProvider>
+      <Analytics />
       <Layout
         className={`${fonts?.overpass?.variable} ${fonts?.overpassMono?.variable} ${fonts?.jsMath?.variable}`}
       >
