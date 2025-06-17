12a9c4d89f301695afad604f557b3aa7141bbe02
DACbyJS <hello@jakobsmith.com>
2025-06-16 21:34:20 -0600

    style: standardize import statements and improve formatting in Footer component

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,37 +1,51 @@
 // Next
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 // Image
-import tuka from '../../public/png/TUKA.png';
-import SvgJakobSmithLogo from '../../components/svg/JakobSmithLogo';
-import WigglyLink from '../../components/svg/WigglyLink';
+import tuka from "../../public/png/TUKA.png";
+import SvgJakobSmithLogo from "../../components/svg/JakobSmithLogo";
+import WigglyLink from "../../components/svg/WigglyLink";
+
+// Constants
+import { pageData } from "../../lib/constants/pageData";
 
 // Style
-import twConfig from '../../tailwind.config';
+import twConfig from "../../tailwind.config";
 
 export default function Footer() {
-  const jsBlack = twConfig.theme.colors['js-black'];
+  const jsBlack = twConfig.theme.colors["js-black"];
   return (
     <footer className="footer bg-js-white relative flex flex-row justify-center md:justify-between items-center white-black-border w-full z-20 py-10 px-10">
       <div className="logo hidden xl:block max-w-[275px]">
-        <SvgJakobSmithLogo pathFill={jsBlack} className={'rainbow-svg-on-hover path-black'}/>
+        <SvgJakobSmithLogo
+          pathFill={jsBlack}
+          className={"rainbow-svg-on-hover path-black"}
+        />
       </div>
       <div className="contact-links text-[1.2ch] md:text-[2ch] flex flex-col items-center justify-center">
-        <Link className="overpass-underline-no-line wiggly-svg-link mb-4" href="https://calendly.com/hello-jakob-smith/">
+        <Link
+          className="overpass-underline-no-line wiggly-svg-link mb-4"
+          href={pageData[8]?.data?.href}
+        >
           Contact Me
-          <WigglyLink/>
+          <WigglyLink />
         </Link>
         <Link
+          target="_blank"
           className="overpass-underline-no-line wiggly-svg-link mb-4"
           href="https://www.linkedin.com/in/jakob-smith-webdev/"
         >
-          linkedin.com/jakobsmithdev
-          <WigglyLink/>
+          Linkedin
+          <WigglyLink />
         </Link>
-        <Link className="overpass-underline-no-line wiggly-svg-link" href="https://github.com/DACbyJS">
-          github.com/dacbyjs
-          <WigglyLink/>
+        <Link
+          target="_blank"
+          className="overpass-underline-no-line wiggly-svg-link"
+          href="https://github.com/DACbyJS"
+        >
+          Github
+          <WigglyLink />
         </Link>
       </div>
       <div className="user relative flex-1 hidden md:block h-[150px] max-w-[375px]">
